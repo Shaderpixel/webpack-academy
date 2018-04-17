767da65b660974d8973714236cd8bd3764435491
Richard Lock <lock.j.h@gmail.com>
2018-04-17 16:58:42 -0500

    Set up output path properties in webpack.config.js using path module

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 /**
  * Created by rlock on 4/17/18.
  */
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+
     }
 }
