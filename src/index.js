c2f991c17ae2b1cc1ee7e328df62bcec3354cd94
Richard Lock <lock.j.h@gmail.com>
2018-04-18 22:06:12 -0500

    Add file-loader and image-webpack-loader. file-loader emits files to the hard disk while image-webpack-laoder allows transformations on image files such as iamge compressions Setup plugins property in webpack.config.js and add ExamplePlugin.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fooString from './foo';
 import bar from './bar-cjs';
+import './shaderpixel-logo.jpg';
 
 console.log(fooString, bar);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
  * Created by rlock on 4/17/18.
  */
 const path = require('path');
+const ExamplePlugin = require('./ExamplePlugin.js');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -9,6 +11,27 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-
-    }
+    },
+		module: {
+    		rules: [
+						{
+								test: /\.jpe?g$/,
+								use: [
+										'file-loader',
+										{
+												loader: 'image-webpack-loader',
+												options: {
+													mozjpeg: {
+															progressive: true,
+															quality: 50
+													}
+												}
+										}
+								]
+						}
+				]
+		},
+		plugins: [
+			new ExamplePlugin(),
+		]
 }
