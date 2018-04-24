53492e367dc81e3facf72e0d53eeadba8c89a621
Richard Lock <lock.j.h@gmail.com>
2018-04-24 15:07:37 -0500

    Added file-loader and url-loader to base64 inline assets below specified file size limit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import createCard from './card'; // importing a module into the dependency graph
+import './shaderpixel-logo.png'; // importing an image to showcase url-loader and file-loader
 
 createCard(
 		"I'm learning webpack: Web Essentials!!!",
